27015ff0b364bfc39d56133b5001715e5854839e
klopspafaf <74278077+klopspafaf@users.noreply.github.com>
2024-10-09 16:06:26 +0700

    Очищен черновик, добавлены уравнения лямбда схемы.

--- usercfg.mk
+++ usercfg.mk
@@ -12,7 +12,7 @@
 ## -pdflua=lualatex with dvi  (faster than -lualatex)
 ## -lualatex=lualatex without dvi
 
-# DRAFTON ?= # 1=on;0=off
+DRAFTON ?=1 # 1=on;0=off
 # SHOWMARKUP ?= # 1=on;0=off
 # FONTFAMILY ?= # 0=CMU;1=MS fonts;2=Liberation fonts
 # ALTFONT ?= # 0=Computer Modern;1=pscyr;2=XCharter
